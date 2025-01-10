12b8558e9aa999174c9a152a47c4dba034d3eda7
Vlad Pavliuk <vladpavliuk@gmail.com>
2025-01-10 20:18:46 +0100

    dictionary added

--- process.php
+++ process.php
@@ -1,14 +1,19 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if (isset($_POST['country'])) {
     $country = trim($_POST['country']);
-    if (!empty($country)) {
-        $filename = 'countries.txt';
-        $countries = file($filename, FILE_IGNORE_NEW_LINES);
-        if (!in_array($country, $countries)) {
-            file_put_contents($filename, $country . PHP_EOL, FILE_APPEND);
-        }
+    $dictionary = file('dictionary.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $countries = file('countries.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    if (in_array($country, $dictionary) && !in_array($country, $countries)) {
+        file_put_contents('countries.txt', $country . PHP_EOL, FILE_APPEND);
     }
-    header('Content-Type: application/json');
-    echo json_encode(file($filename, FILE_IGNORE_NEW_LINES));
+
+    $countries = file('countries.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $options = '<option value="">Select a country</option>';
+    foreach ($countries as $c) {
+        $options .= '<option value="' . htmlspecialchars($c) . '">' . htmlspecialchars($c) . '</option>';
+    }
+    echo $options;
 }
+?>
 
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-$(document).ready(function() {
-    $('#countryForm').on('submit', function(event) {
-        event.preventDefault();
-        var country = $('#country').val();
+
+    $(document).ready(function() {
+    $('#countryForm').on('submit', function(e) {
+        e.preventDefault();
+        var country = $('#countryInput').val().trim();
         if (country) {
             $.ajax({
                 url: 'process.php',
                 type: 'POST',
                 data: { country: country },
-                success: function(data) {
-                    $('#countries').empty();
-                    $.each(data, function(index, value) {
-                        $('#countries').append('<option>' + value + '</option>');
-                    });
+                success: function(response) {
+                    if (response) {
+                        $('#countrySelect').html(response);
+                    } else {
+                        alert('Country already exists or is invalid.');
+                    }
                 }
             });
         }
